ec6901b470dd4b74c28c20c9d127264fab2b4774
rarm <rarm.jet@gmail.com>
2020-10-05 13:19:05 +1100

    sanitize posixpath to string

--- imagelookup/crud/image.py
+++ imagelookup/crud/image.py
@@ -10,9 +10,9 @@ def translate_name_to_filename(name: str = None):
     if name is not None:
         filename = name.lower().replace(" ", "_") + ".jpg"
         filename = sanitize_filename(filename)
-        logger.info("File Found" + filename)
+        logger.info("File Found" + str(filename))
     else:
-        logger.error("File not found Found", filename=-filename)
+        logger.error("File not found Found", filename=str(name))
         filename = "error.png"
 
     return filename
@@ -24,13 +24,13 @@ def get_image_as_base64(name: str = None):
         filename = translate_name_to_filename(name)
         filepath = str(pathlib.Path.cwd().joinpath("static", "images", filename))
         image_path = pathlib.Path(filepath)
-        logger.debug("Imagepath: " + image_path)
+        logger.debug("Imagepath: " + str(image_path))
         if image_path.is_file() and imghdr.what(image_path) is not None:
             with open(image_path, "rb") as img_file:
                 image_base64 = base64.b64encode(img_file.read()).decode("utf-8")
             return image_base64
         else:
-            logger.error("Imagepath not image", Imagepath=image_path)
+            logger.error("Imagepath not image", Imagepath=str(image_path))
             raise ValueError
     else:
         logger.error("Nnvlid name provided", name=name)
